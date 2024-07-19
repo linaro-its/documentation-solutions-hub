eee07aa3224e6bdf0d993ab5fda05e43b322e21d
Philip Colmer <philip.colmer@linaro.org>
2024-07-19 08:25:50 +0100

    Adjust project details

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Linaro Solutions Hub'
+copyright = '2024 Linaro'
+author = 'Philip Colmer'
 
 release = '0.1'
 version = '0.1.0'
